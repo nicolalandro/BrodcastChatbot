db63069a959f35068ee49e548c26afe8b5240a09
nicolalandro <nicolaxx94@live.it>
2018-05-31 12:49:25 +0200

    post brodcast to subscriber

--- src/http_server.js
+++ src/http_server.js
@@ -3,16 +3,29 @@ const smarketbot = require('./telegram_bot/telegram_bot');
 const bot = new smarketbot('telegram_bot/token');
 
 
-http.createServer(function (request, response) {
-    if (request.method == 'POST') {
+const server = http.createServer();
+
+server.on('request', (request, response) => {
+    const { headers, method, url } = request;
+    if (method == 'POST') {
+        let body = [];
+        request.on('error', (err) => {
+            console.error(err);
+        }).on('data', (chunk) => {
+            body.push(chunk);
+        }).on('end', () => {
+            body = Buffer.concat(body).toString();
+            bot.brodcastMessage(body);
+        });
         response.writeHead(200, {'Content-Type': 'text/plain'});
-        response.end('Hello Post\n');
+        response.end('Done\n');
     }
     else {
         response.writeHead(200, {'Content-Type': 'text/plain'});
-        response.end('Hello World\n');
+        response.end('You Made a GET\n');
     }
-}).listen(8081);
+});
 
+server.listen(8081);
 // Console will print the message
 console.log('Server running at http://127.0.0.1:8081/');
