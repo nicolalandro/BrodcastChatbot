0a42fe2b608ff064f7e7efea6f87f64443ed2fc2
nicolalandro <nicolaxx94@live.it>
2018-05-31 12:06:00 +0200

    working both with fake root

---
+++ src/html_server.js
@@ -0,0 +1,12 @@
+const http = require("http");
+const smarketbot = require('./telegram_bot/telegram_bot');
+const bot = new smarketbot('telegram_bot/token');
+
+
+http.createServer(function (request, response) {
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+    response.end('Hello World\n');
+}).listen(8081);
+
+// Console will print the message
+console.log('Server running at http://127.0.0.1:8081/');
